f086dd45ff5a04b10d6802449db773a0712761ac
Paulin Trognon <paulin.trognon@gmail.com>
2017-10-01 21:03:14 +0200

    move all to subfolder

---
+++ list/index.js
@@ -0,0 +1,15 @@
+module.exports = [
+  {
+    name: 'Classical Music',
+    link: 'https://en.wikipedia.org/wiki/Classical_music',
+    subgenres: require('./classical-music'),
+  },
+  {
+    name: 'Popular Music',
+    link: 'https://en.wikipedia.org/wiki/Popular_music',
+  },
+  {
+    name: 'Traditional Music',
+    link: 'https://en.wikipedia.org/wiki/Traditional_music',
+  }
+];

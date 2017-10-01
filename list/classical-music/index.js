afc1bfca606b25ebb7ba3759bd27f417bf98b258
Paulin Trognon <paulin.trognon@gmail.com>
2017-10-01 22:18:35 +0200

    fix

--- list/classical-music/index.js
+++ list/classical-music/index.js
@@ -2,10 +2,11 @@ module.exports = [
   {
     name: 'Ancient Music',
     link: 'https://en.wikipedia.org/wiki/Ancient_music',
-    subgenres: require('./ancient-music')
+    subgenres: require('./ancient-music'),
   },
   {
     name: 'Medieval Music',
     link: 'https://en.wikipedia.org/wiki/Medieval_music',
+    subgenres: require('./medieval-music'),
   },
 ];

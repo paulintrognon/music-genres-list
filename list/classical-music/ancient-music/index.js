75531a89fa08fe7bc8eb19cf85ee69f34fd70a12
Paulin Trognon <paulin.trognon@gmail.com>
2017-10-02 13:45:43 +0200

    add examples

--- list/classical-music/ancient-music/index.js
+++ list/classical-music/ancient-music/index.js
@@ -1,10 +1,16 @@
 module.exports = [
   {
-    name: 'Music of Ancient Egypt',
-    link: 'https://en.wikipedia.org/wiki/Music_of_Egypt#History',
+    name: 'Music of Ancient Greece',
+    link: 'https://en.wikipedia.org/wiki/Music_of_ancient_Greece',
     examples: [
-      'https://www.youtube.com/watch?v=nBmWXmn11YE',
-      'https://www.youtube.com/watch?v=Ava9H3BVICM',
+      'https://www.youtube.com/watch?v=elERNFoEf3Y',
+    ],
+  },
+  {
+    name: 'Music of Ancient Rome',
+    link: 'https://en.wikipedia.org/wiki/Music_of_ancient_Rome',
+    examples: [
+      'https://www.youtube.com/watch?v=X83IYWmcEFg',
     ],
   },
   {
@@ -12,6 +18,14 @@ module.exports = [
     link: 'https://en.wikipedia.org/wiki/Music_of_Mesopotamia',
     examples: [
       'https://www.youtube.com/watch?v=2H8_13x3JaI',
+    ],
+  },
+  {
+    name: 'Music of Ancient Egypt',
+    link: 'https://en.wikipedia.org/wiki/Music_of_Egypt#History',
+    examples: [
+      'https://www.youtube.com/watch?v=nBmWXmn11YE',
+      'https://www.youtube.com/watch?v=Ava9H3BVICM',
     ],
   },
   {
@@ -27,16 +41,5 @@ module.exports = [
     examples: [
       'https://www.youtube.com/watch?v=zfgqHwBdsXw',
     ],
-  },
-  {
-    name: 'Music of Ancient Greece',
-    link: 'https://en.wikipedia.org/wiki/Music_of_ancient_Greece',
-    examples: [
-      'https://www.youtube.com/watch?v=elERNFoEf3Y',
-    ],
-  },
-  {
-    name: 'Music of Ancient Rome',
-    link: 'https://en.wikipedia.org/wiki/Music_of_ancient_Rome',
   },
 ];
--- list/classical-music/index.js
+++ list/classical-music/index.js
@@ -3,10 +3,18 @@ module.exports = [
     name: 'Ancient Music',
     link: 'https://en.wikipedia.org/wiki/Ancient_music',
     subgenres: require('./ancient-music'),
+    examples: [
+      'https://www.youtube.com/watch?v=elERNFoEf3Y',
+      'https://www.youtube.com/watch?v=X83IYWmcEFg',
+    ],
   },
   {
     name: 'Medieval Music',
     link: 'https://en.wikipedia.org/wiki/Medieval_music',
     subgenres: require('./medieval-music'),
+    examples: [
+      'https://www.youtube.com/watch?v=4qcO1RlGh-A',
+      'https://www.youtube.com/watch?v=qKNG6EjiIsE',
+    ],
   },
 ];

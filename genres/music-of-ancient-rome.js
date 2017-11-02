151eb3f8421c7e3246e8c2aae07770b006ecd441
Paulin Trognon <paulin.trognon@gmail.com>
2017-11-02 14:04:02 +0100

    fix

--- genres/music-of-ancient-rome.js
+++ genres/music-of-ancient-rome.js
@@ -1,7 +1,5 @@
 module.exports = {
-  name: 'Music of Ancient Greece',
-  link: 'https://en.wikipedia.org/wiki/Music_of_ancient_Greece',
-  examples: [
-    'https://www.youtube.com/watch?v=elERNFoEf3Y',
-  ],
+  name: 'Music of Ancient Rome',
+  link: 'https://en.wikipedia.org/wiki/Music_of_ancient_Rome',
+  examples: [],
 };

1d54b80c949faf808b0087f1dea913081bf8c908
Paulin Trognon <paulin.trognon@gmail.com>
2017-12-08 15:16:23 +0100

    change broken links

--- genres/acid-techno.js
+++ genres/acid-techno.js
@@ -3,7 +3,7 @@ module.exports = {
   link: 'https://en.wikipedia.org/wiki/Acid_techno',
   examples: [
     'https://www.youtube.com/watch?v=Xa9HFV5z1p4',
-    'https://www.youtube.com/watch?v=LSkeu9xLB-Q',
+    'https://www.youtube.com/watch?v=G8IwGUR8S54',
     'https://www.youtube.com/watch?v=VotISSj1GS4',
   ],
   parents: [
--- genres/motet-renaissance.js
+++ genres/motet-renaissance.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'Motet (Renaissance)',
   link: 'https://en.wikipedia.org/wiki/Motet#Renaissance_examples',
   examples: [
-    'https://www.youtube.com/watch?v=PQo_LirQY-k',
+    'https://www.youtube.com/watch?v=7mKV-2c5HDQ',
     'https://www.youtube.com/watch?v=O4ma4m8yhyE',
     'https://www.youtube.com/watch?v=LxK8zd2hLEM',
   ],
